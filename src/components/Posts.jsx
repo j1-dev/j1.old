851e26ba3411b47387ef60eb93aff33a6f04bc40
j1-dev <aephyxen@gmail.com>
2023-02-10 10:27:42 +0100

    Pagination v2 with loadMorePosts on scroll

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -64,6 +64,7 @@ const Posts = ({ path, className }) => {
   const [posts, setPosts] = useState([]);
   const [limite, setLimite] = useState(5);
   const [loading, setLoading] = useState(false);
+  const [atBottom, setAtBottom] = useState(false);
 
   useEffect(() => {
     const unsub = () => {
@@ -88,7 +89,33 @@ const Posts = ({ path, className }) => {
   const handleLoadMore = () => {
     setLoading(true);
     setLimite(limite + 5);
+    setAtBottom(false);
   };
+
+  const handleScroll = () => {
+    if (loading || atBottom) return;
+    if (
+      window.innerHeight + document.documentElement.scrollTop >=
+      document.documentElement.offsetHeight
+    ) {
+      console.log("Scrolled to bottom!");
+      setAtBottom(true);
+      handleLoadMore();
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [posts]);
+
+  useEffect(() => {
+    if (!loading && atBottom) {
+      setAtBottom(false);
+    }
+  }, [loading, atBottom]);
 
   return (
     <div className="post-feed text-center">
@@ -97,13 +124,7 @@ const Posts = ({ path, className }) => {
           <Post data={doc} path={path} key={doc.id} className={className} />
         );
       })}
-      {loading ? (
-        <p>Loading...</p>
-      ) : (
-        <button className="load-more-button" onClick={handleLoadMore}>
-          Load More
-        </button>
-      )}
+      {loading && <p>Loading...</p>}
     </div>
   );
 };
