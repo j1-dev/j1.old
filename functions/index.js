eab0894e69eb322fb489f2bab8948adbd7f4653b
j1-dev <aephyxen@gmail.com>
2023-02-18 12:18:34 +0100

    Clean up for going public!

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,17 @@
+// // The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
+// const functions = require('firebase-functions');
+
+// // The Firebase Admin SDK to access Firestore.
+// const admin = require('firebase-admin');
+// admin.initializeApp();
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+// //...
+// exports.notificationLike = functions.firestore
+//   .document('Posts/{PostId}/Likes')
+//   .onCreate((snap, context) => {
+//     const post = snap.data();
+//     //stuff
+//   });
+// //...
