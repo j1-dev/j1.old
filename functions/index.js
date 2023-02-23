d0b76e1741981236ac7406860ec124da8f626084
j1-dev <aephyxen@gmail.com>
2023-02-23 12:56:52 +0100

    :fire: Back to square one
    No idea how to check when a document has been created in a deeply nested subcollection. Maybe will just check the Posts while I'm still researching how to check subcollections at a deeper level

--- functions/index.js
+++ functions/index.js
@@ -1,52 +1,13 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info('Hello logs!', { structuredData: true });
-  response.send('Hello from Firebase!');
-});
-
-exports.notificationLike = functions.firestore
-  .document('Posts/{PostId}/Likes/{LikeId}')
+exports.test2 = functions.firestore
+  .document('Posts/{PostId}/{Comments}/{LikedPostId}/Likes/{LikeId}')
   .onCreate((snap, context) => {
-    // const post = snap.data();
-    // const now = new Date();
-    // const time = Math.round(now.getTime() / 1000);
-    const postId = context.params.PostId;
-    const likeId = context.params.LikeId;
-    const likedPost = admin.firestore.doc(`Posts/id/**/Posts/${postId}`);
-    return listenToPostsSubcollections(likedPost);
-
-    // const notification = {
-    //   createdAt: time,
-    // };
-    //stuff
+    functions.logger.info('aaaaaaaaaaaaaaaaaaaaa' + snap);
   });
-
-function listenToPostsSubcollections(postRef) {
-  return postRef.listCollections().then((collections) => {
-    collections.forEach((collection) => {
-      if (collection.id === 'Posts') {
-        // Listen to changes in the Posts subcollection
-        collection.onSnapshot((snapshot) => {
-          snapshot.docChanges().forEach((change) => {
-            const postId = change.doc.id;
-            const data = change.doc.data();
-            console.log(`Post ${postId} was changed:`, data);
-            // Do something with the changed data
-          });
-        });
-      } else {
-        // Recursively listen to changes in subcollections of this collection
-        collection.onSnapshot((snapshot) => {
-          snapshot.docChanges().forEach((change) => {
-            const docRef = change.doc.ref;
-            listenToPostsSubcollections(docRef);
-          });
-        });
-      }
-    });
-  });
-}
